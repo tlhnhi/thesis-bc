962294f6125a328a68a5812b755f2c050e01d71a
tlhnhi <tlhnhitn@gmail.com>
2021-07-23 11:47:16 +0700

    edit main route, update ui

--- demo_app.py
+++ demo_app.py
@@ -4,7 +4,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+@app.route('/')
+def main():
+    return render_template('index.html')
