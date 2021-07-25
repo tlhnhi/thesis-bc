74df5fde392bfe0f7d41a47565ea798d9686abb2
tlhnhi <tlhnhitn@gmail.com>
2021-07-25 18:32:08 +0700

    use layout, read image

--- demo_app.py
+++ demo_app.py
@@ -1,9 +1,36 @@
-from flask import render_template
+from flask import Flask, request, redirect, render_template
+from PIL import Image
+from io import BytesIO
+from base64 import b64encode
 
-from flask import Flask
+
+def get_prediction(img_bytes):
+    image = Image.open(BytesIO(img_bytes))
+
+    img_output = BytesIO()
+    image.save(img_output, format='JPEG')
+    img_bytes = img_output.getvalue()
+
+    image = b64encode(img_bytes).decode("utf-8")
+
+    preds = None
+
+    return preds, image
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def main():
-    return render_template('index.html')
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        post_file = request.files.get('file')
+        if not post_file:
+            return redirect(request.url)
+        img_bytes = post_file.read()
+        
+
+        preds, out_img = get_prediction(img_bytes)
+        return render_template('result.html', preds=preds, image=out_img)
+    else:
+	    return render_template('index.html')
